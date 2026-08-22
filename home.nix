f90006b77001b974be796ccea778ef16fe9b9083
Centari-Alpha <djbrodka@gmail.com>
2026-08-22 03:04:33 +0000

    additional alias commands for zsh

--- home.nix
+++ home.nix
@@ -24,6 +24,21 @@ in
   fonts.fontconfig.enable = true;
   home.sessionVariables.EDITOR = "nvim";
 
+  programs.zsh = {
+    enable = true
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+    initContent = ''
+      bindkey '^f' autosuggest-accept
+    '';
+    shellAliases = {
+      ".." = "cd ..";
+      add = "git add .";
+      push = "git push";
+      pull = "git pull";
+      m = "git switch main";
+  };
+
   home.file.".config/wezterm".source =
     config.lib.file.mkOutOfStoreSymlink "${dotfiles}/home/.config/wezterm";
 
