ba1076364364615d03ad0a79cddb4b281e906550
Centari-Alpha <djbrodka@gmail.com>
2026-08-22 00:09:14 +0000

    added git and adjusted users to config

--- configuration.nix
+++ configuration.nix
@@ -6,7 +6,12 @@
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   wsl.enable = true;
-  wsl.defaultUser = "nixos";
+  wsl.defaultUser = "Centari";
+
+  users.users.Centari = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; 
+  }; 
 
   environment.systemPackages = with pkgs; [
     git vim wget
@@ -14,12 +19,18 @@
 
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
-  home-manager.users.nixos = { pkgs, ... }: {
+  home-manager.users.Centari = { pkgs, ... }: {
     home.stateVersion = "24.05"; 
     home.packages = [ pkgs.htop 
 		      pkgs.fastfetch
 		      pkgs.ripgrep
     ];
+    
+    programs.git = {
+      enable = true;
+      userName = "Centari-Alpha";
+      userEmail = "djbrodka@gmail.com";
+    };
   };
 
   system.stateVersion = "26.05";
