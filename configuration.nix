9b44a40aa3af71e4e6691c457108943c4f3def9d
Centari-Alpha <djbrodka@gmail.com>
2026-08-22 02:44:38 +0000

    adjust config for flakes

--- configuration.nix
+++ configuration.nix
@@ -17,21 +17,5 @@
     git vim wget
   ];
 
-  home-manager.useGlobalPkgs = true;
-  home-manager.useUserPackages = true;
-  home-manager.users.Centari = { pkgs, ... }: {
-    home.stateVersion = "24.05"; 
-    home.packages = [ pkgs.htop 
-		      pkgs.fastfetch
-		      pkgs.ripgrep
-    ];
-    
-    programs.git = {
-      enable = true;
-      userName = "Centari-Alpha";
-      userEmail = "djbrodka@gmail.com";
-    };
-  };
-
   system.stateVersion = "26.05";
 }
